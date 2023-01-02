0f082101a0edad3d2111c3af9267ab1f7172688d
the-bearded-nerd <dmitrykarvonen@gmail.com>
2023-01-02 21:19:38 +0300

    feat: add persist functionality

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 import App from "./App";
 // import { CartProvider } from "./context/cart.context";
-import { store } from "./store/store";
+import { store, persistor } from "./store/store";
 
 import "./index.scss";
 
@@ -14,9 +15,9 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        {/* <CartProvider> */}
-        <App />
-        {/* </CartProvider> */}
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
       </Provider>
     </BrowserRouter>
   </React.StrictMode>
--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,24 @@
 import { compose, createStore, applyMiddleware } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import { rootReducer } from "./root-reducer";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: ["user"],
+};
+
+const persistedReduser = persistReducer(persistConfig, rootReducer);
 
 const middleWares = [];
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+export const store = createStore(
+  persistedReduser,
+  undefined,
+  composedEnhancers
+);
+
+export const persistor = persistStore(store);
